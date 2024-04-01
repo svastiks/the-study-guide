68f0edaebc21889cfcd1186a14294b9acd79f1a7
Svastik <svastiksharma13@gmail.com>
2024-03-31 23:07:12 -0400

    components created

---
+++ client/src/Components/Landing/Landing.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Landing.css'
+
+const Landing = () => {
+  return (
+    <div>
+
+        <div className='container'>
+
+            <form onSubmit={''}>
+
+                <input type="text" placeholder='What do you want to learn today?' />
+
+            </form>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Landing
